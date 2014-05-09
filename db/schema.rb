6ed96ea925c58975663c4e3671767a748a145e75
johncallahan <jcallahan@acm.org>
2014-05-09 13:55:53 +0100

    reroute to displayqr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509030756) do
+ActiveRecord::Schema.define(version: 20140509121244) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20140509030756) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                              default: "",  null: false
+    t.string   "encrypted_password",     limit: 128, default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20140509030756) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gauth_secret"
+    t.string   "gauth_enabled",                      default: "f"
+    t.string   "gauth_tmp"
+    t.datetime "gauth_tmp_datetime"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
